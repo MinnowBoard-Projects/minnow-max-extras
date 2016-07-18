e2eaa10037d1a2cae3bb44568501c66784b19637
John 'Warthog9' Hawley <john.hawley@intel.com>
2016-07-18 16:30:05 -0700

    Deleteing the off function and adding some minor documentation

--- onboard/d2-led.sh
+++ onboard/d2-led.sh
@@ -26,10 +26,6 @@ led1() {
 	echo $1 > /sys/class/gpio/gpio$LED1/value
 }
 
-off() {
-	led1 0
-}
-
 if [ \
 	! -d /sys/class/gpio/gpio$LED1 ]; then
 	echo $LED1 > /sys/class/gpio/export
@@ -45,6 +41,11 @@ echo out > /sys/class/gpio/gpio$LED1/direction
 
 i=0
 while true ; do
+	#
+	# Also noting this LED is going to act "backwards" to likely what you are expecting
+	# as a value of 1 will be off and a value of 0 will be on, as the LED has a not-gate
+	# involved in it to help it match the original functionality of the LED
+	#
 	led1 $((i%2))
 	sleep 1
 	i=$((i+1))
