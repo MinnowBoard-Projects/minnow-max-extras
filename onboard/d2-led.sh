25d979a3c9c9237045c46adfa7224bdd05644e45
John 'Warthog9' Hawley <john.hawley@intel.com>
2016-07-18 15:21:03 -0700

    Adding in the offset checker already present in most everything else

--- onboard/d2-led.sh
+++ onboard/d2-led.sh
@@ -3,12 +3,26 @@
 #
 # Use GPIO 104 if you are on a kernel older than 3.18
 # Use GPIO 360 if you are on a kernel newer than 3.17
-# Defaulting to the later at this point
-#
-#LED1=104
-LED1=360
 
-led1() {
+LED1=104
+
+# Between Linux kernel 3.17 and 3.18 there was a change in how GPIOs are enumerated
+# specifically instead of counting down from 256, they now count down from 512.
+# This means that all GPIOs are now offset by an additional 256 in newer kernels
+# so the following code attempts to handle that
+#
+OFFSET="0"
+
+KERNEL_MAJOR_MINOR=$( uname -r | awk 'BEGIN { FS="."; } ; { print $1 "." $2; }' )
+
+if [[ ${KERNEL_MAJOR_MINOR} > 3.17 ]]
+then
+	OFFSET="256"
+fi
+
+LED1=$(( ${LED1} + ${OFFSET} ))
+
+led1(
 	echo $1 > /sys/class/gpio/gpio$LED1/value
 }
 
